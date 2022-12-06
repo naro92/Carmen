4346df95d72b65ce2fee71906837f213b9249d7c
Tugdual de K <35851118+Thug2000@users.noreply.github.com>
2022-12-06 20:34:46 +0100

    Controlleur Connexion
    Ajout des fonctions de connexion pour les medecins patients et familles
    Ajout d'une fonction de deconnexion

--- app/controllers/Connexion.php
+++ app/controllers/Connexion.php
@@ -35,15 +35,77 @@ class Connexion extends Controller {
             $password = hash('sha3-256', $_POST['password']);
         }
 
-        // echo $email;
-        // echo $password;
-
-        $patient = $this->model('Patient');
+        $patient = $this->model('PatientModel');
 
         $bdd = new PDO('mysql:host=localhost:3306;dbname=mydb;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         $patient->connexionPatient = $patient->connexionPatient($bdd, $email, $password);
 
-        echo $patient->connexionPatient;
+        if($patient->connexionPatient){
+            echo "Connection is successful !";
+            session_start();
+		    $_SESSION['user'] = $email;
+            $_SESSION['role'] = "patient";
+		    header('Location: /mvcExample/public/patient');
+		    exit();
+        } else {
+            echo "There was an error !";
+        }
+    }
+
+    public function connexionMedecin(){
+
+        if ((isset($_POST['email']) && !empty($_POST['email'])) && (isset($_POST['password']) && !empty($_POST['password']))) {
+            $email = $_POST['email'];
+            $password = hash('sha3-256', $_POST['password']);
+        }
+
+        $patient = $this->model('MedecinModel');
+
+        $bdd = new PDO('mysql:host=localhost:3306;dbname=mydb;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        $patient->connexionMedecin = $patient->connexionMedecin($bdd, $email, $password);
+
+        if($patient->connexionMedecin){
+            echo "Connection is successful !";
+            session_start();
+		    $_SESSION['user'] = $email;
+            $_SESSION['role'] = "medecin";
+		    header('Location: /mvcExample/public/medecin');
+		    exit();
+        } else {
+            echo "There was an error !";
+        }
+    }
+
+    public function connexionFamille(){
+
+        if ((isset($_POST['email']) && !empty($_POST['email'])) && (isset($_POST['password']) && !empty($_POST['password']))) {
+            $email = $_POST['email'];
+            $password = hash('sha3-256', $_POST['password']);
+        }
+
+        $patient = $this->model('FamilleModel');
+
+        $bdd = new PDO('mysql:host=localhost:3306;dbname=mydb;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        $patient->connexionMedecin = $patient->connexionFamille($bdd, $email, $password);
+
+        if($patient->connexionMedecin){
+            echo "Connection is successful !";
+            session_start();
+		    $_SESSION['user'] = $email;
+            $_SESSION['role'] = "famille";
+		    header('Location: /mvcExample/public/medecin');
+		    exit();
+        } else {
+            echo "There was an error !";
+        }
+    }
+
+    public function deconnexion(){
+        session_start();
+        session_unset();
+        session_destroy();
+        header('Location: /mvcExample/public');
+        exit();
     }
 
 }
