c1f527851e78eb18c5482f144992907ca7b5aa65
Tugdual de K <35851118+Thug2000@users.noreply.github.com>
2022-12-06 20:35:52 +0100

    Create FamilleModel.php

---
+++ app/models/FamilleModel.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Classe familleModel
+ * 
+ * Model famille
+ * 
+ * Permet de gerer les données des familles
+ */
+class FamilleModel {
+
+    public function connexionFamille(PDO $bdd, String $email, String $password){
+        $query = 'SELECT * FROM famille WHERE adresse_mail="'.$email.'" AND mdp="'.$password.'"';
+        $params = array();
+        $return = '';
+        $statement = $bdd->prepare($query);
+        $statement->execute($params);
+        $count = $statement->rowCount();
+        if($count > 0){
+            $connectionSuccessful = 1;
+        } else {
+            $connectionSuccessful = 0;
+        }
+                     
+	    return $connectionSuccessful;
+    }
+
+    public function inscriptionFamille(PDO $bdd, String $nom, String $email, String $password){
+        $query = 'SELECT * FROM patient WHERE adresse_mail="'.$email.'" ';
+
+		$params = array();
+        $statement = $bdd->prepare($query);
+        $statement->execute($params);
+        $count = $statement->rowCount();
+        if($count > 0){
+			// S'il est deja utilisé on dit que la personne est deja inscrite
+            return "Un utilisateur possède déjà votre adresse mail !";
+        } else {
+			// Sinon on ajoute toutes les données dans la database
+			$sql = "INSERT INTO patient(adresse_mail, nom, mdp) VALUES (?, ?, ?)";
+			$stmt= $bdd->prepare($sql);
+        	$exec = $stmt->execute([$email, $nom, $password]);
+            if($exec){
+                $return = "inscription successful !";
+            } else {
+                $return = "Il y a une erreur";
+            }
+		}
+
+        return $return;
+    }
+}
