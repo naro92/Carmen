def1f597085644a248e31adf1c60c0406adb67d3
Mathischmp <mathischampagne@yahoo.fr>
2022-12-15 23:04:12 +0100

    Add files via upload

---
+++ rechercher_patient.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="rechercher_patient.css">
+    <title></title>
+
+</head>
+<body>
+<div class = "bandeau">
+        <div class="logo-container">
+           <img src="logo.png" />
+           <a href="#">Carmen</a>
+        </div>
+        <ul class="nav-list" id="nav-list">
+        <li class="list-item">
+          <a href="#cgu">CGU</a>
+        </li>
+        <li class="list-item">
+          <a href="#faq">Contacts</a>
+        </li>
+        <li class="list-item">
+          <a href="#contact">Déconnexion</a>
+        </li>
+      </ul>
+</div>
+<div class = "btn_retour">
+	<a href=".\ajouter_patient_medecin.php">	&#10229 Retour</a>
+</div>
+<div class="title_container">
+<h1>Rechercher un patient<h1>
+</div>
+<div class="main_container">
+<div class="main">
+<form action="rechercher.php" method="post">
+		<pre>
+			<label>Nom</label><input type="text" name="nom">
+			<label>Prénom</label><input type="text" name="prenom">
+			<label>e-Mail</label><input type="email" name="email">
+			<input id="btn_rechercher" type="submit" value="Rechercher">
+		</pre>
+</form>
+</div>
+<div class=text_alert>
+<p>Veuillez remplir au minmimum un champ<p>
+</div>
+</div>
+<div class="footer-section">
+      <!-- Section -->
+      <hr />
+      <div class="footer-container">
+        <a class="item-footer" href="#contact">Contact</a>
+        <a class="item-footer" href="#cgu">
+          Conditions générales d'utilisations
+        </a>
+        <a class="item-footer" href="#faq">FAQ</a>
+      </div>
+    </div>
+
+</body>
+</html>
