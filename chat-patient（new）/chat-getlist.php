a1494663a53c757a57747519157c308ae0188b23
liu-weida <117468158+liu-weida@users.noreply.github.com>
2023-01-12 09:27:30 +0100

    Add files via upload

---
+++ chat-patient（new）/chat-getlist.php
@@ -0,0 +1,39 @@
+<?php
+
+//显示聊天内容Afficher le contenu du chat
+
+require("./chat-class-function.php");
+
+$db = db::getdb();
+
+
+$nom = $db->getpatientnom();
+
+$pi = $db->getpatientID($nom);
+
+$mi = $db->getmedecinID($pi);
+
+$list = db::getdb()->getlts($pi,$mi);
+
+$listNom = $db->getnom($pi);
+
+$str='';
+
+if($list){
+
+for($i=0,$k=count($list);$i<$k;$i++) //通过循环逐条获取Passe par les boucles une par une
+{
+
+$str .= "<span style='color:#C9E265;'>".$list[$i]['time']."</span>
+
+<br />
+
+".$list[$i]['contenu'].
+
+"<br />";
+
+}
+}
+echo $str;
+
+?>
